701f2440220e628bf4031a6ed4af3ec0b7f5677a
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2015-03-22 22:49:45 +0000

    #18 Instant serialization tests

--- src/test/java/com/fasterxml/jackson/datatype/jsr310/TestInstantKeySerialization.java
+++ src/test/java/com/fasterxml/jackson/datatype/jsr310/TestInstantKeySerialization.java
@@ -1,15 +1,21 @@
 package com.fasterxml.jackson.datatype.jsr310;
 
+import static org.junit.Assert.assertEquals;
+
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestInstantKeySerialization {
+
+    private static final String INSTANT_STRING = "2011-07-19T21:35:11.000000123Z";
+    private static final Instant INSTANT = Instant.ofEpochSecond(1311111311l, 123);
 
     private ObjectMapper om;
     private Map<Instant, String> map;
@@ -26,15 +32,39 @@ public class TestInstantKeySerialization {
      */
 
     @Test
-    public void testSerialization() {
-        // TODO test
-        Assert.fail("Not done yet");
+    public void testSerialization0() throws Exception {
+        map.put(Instant.ofEpochMilli(0), "test");
+
+        String value = om.writeValueAsString(map);
+
+        Assert.assertEquals("Value is incorrect", map("1970-01-01T00:00:00Z", "test"), value);
     }
 
     @Test
-    public void testDeserialization() {
-        // TODO test
-        Assert.fail("Not done yet");
+    public void testSerialization1() throws Exception {
+        map.put(INSTANT, "test");
+
+        String value = om.writeValueAsString(map);
+
+        assertEquals("Value is incorrect", map(INSTANT_STRING, "test"), value);
+    }
+
+    @Test
+    public void testDeserialization0() throws Exception {
+        Map<Instant, String> value = om.readValue(map("1970-01-01T00:00:00Z", "test"), new TypeReference<Map<Instant, String>>() {
+        });
+
+        map.put(Instant.ofEpochMilli(0), "test");
+        assertEquals("Value is incorrect", map, value);
+    }
+
+    @Test
+    public void testDeserialization1() throws Exception {
+        Map<Instant, String> value = om.readValue(map(INSTANT_STRING, "test"), new TypeReference<Map<Instant, String>>() {
+        });
+
+        map.put(INSTANT, "test");
+        assertEquals("Value is incorrect", map, value);
     }
 
     private String map(String key, String value) {
