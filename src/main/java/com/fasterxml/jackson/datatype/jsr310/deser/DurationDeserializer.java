10db2d17e0bdfe871aad368c62ac2863a57b5ccf
Cowtowncoder <tatu.saloranta@iki.fi>
2015-07-08 16:35:34 -0700

    minor warning cleanup

--- src/main/java/com/fasterxml/jackson/datatype/jsr310/deser/DurationDeserializer.java
+++ src/main/java/com/fasterxml/jackson/datatype/jsr310/deser/DurationDeserializer.java
@@ -17,6 +17,7 @@
 package com.fasterxml.jackson.datatype.jsr310.deser;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonTokenId;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.DecimalUtils;
@@ -45,27 +46,27 @@ public class DurationDeserializer extends JSR310DeserializerBase<Duration>
     @Override
     public Duration deserialize(JsonParser parser, DeserializationContext context) throws IOException
     {
-        switch(parser.getCurrentToken())
+        switch (parser.getCurrentTokenId())
         {
-            case VALUE_NUMBER_FLOAT:
+            case JsonTokenId.ID_NUMBER_FLOAT:
                 BigDecimal value = parser.getDecimalValue();
                 long seconds = value.longValue();
                 int nanoseconds = DecimalUtils.extractNanosecondDecimal(value, seconds);
                 return Duration.ofSeconds(seconds, nanoseconds);
 
-            case VALUE_NUMBER_INT:
+            case JsonTokenId.ID_NUMBER_INT:
                 if(context.isEnabled(DeserializationFeature.READ_DATE_TIMESTAMPS_AS_NANOSECONDS)) {
                     return Duration.ofSeconds(parser.getLongValue());
                 }
                 return Duration.ofMillis(parser.getLongValue());
 
-            case VALUE_STRING:
+            case JsonTokenId.ID_STRING:
                 String string = parser.getText().trim();
-                if(string.length() == 0)
+                if(string.length() == 0) {
                     return null;
+                }
                 return Duration.parse(string);
         }
-
         throw context.mappingException("Expected type float, integer, or string.");
     }
 }
