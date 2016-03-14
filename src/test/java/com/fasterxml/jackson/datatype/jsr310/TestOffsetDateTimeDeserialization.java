57a9043cfa9ff1d795172137af0767ec66b49180
Nicholas Wolff <nwolff@gmail.com>
2016-03-14 14:49:41 +0100

    test of OffsetDateTimeDeserialization when timezone is not Zulu

--- src/test/java/com/fasterxml/jackson/datatype/jsr310/TestOffsetDateTimeDeserialization.java
+++ src/test/java/com/fasterxml/jackson/datatype/jsr310/TestOffsetDateTimeDeserialization.java
@@ -1,6 +1,7 @@
 package com.fasterxml.jackson.datatype.jsr310;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectReader;
 import org.junit.Test;
 
@@ -13,14 +14,33 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
+
 public class TestOffsetDateTimeDeserialization extends ModuleTestBase
 {
-    private final ObjectReader READER = newMapper().readerFor(OffsetDateTime.class);
+   private final ObjectReader READER = newMapper().readerFor(OffsetDateTime.class);
 
     @Test
     public void testDeserializationAsString01() throws Exception
     {
         expectSuccess(OffsetDateTime.of(2000, 1, 1, 12, 0, 0, 0, ZoneOffset.UTC), "'2000-01-01T12:00Z'");
+    }
+
+    @Test
+    public void testDeserializationAsString02() throws Exception
+    {
+        expectSuccess(OffsetDateTime.of(2000, 1, 1, 7, 0, 0, 0, ZoneOffset.UTC), "'2000-01-01T12:00+05:00'");
+    }
+
+    @Test
+    public void testDeserializationAsString03() throws Exception
+    {
+        //
+        // Verify that the offset in the json is preserved when we disable ADJUST_DATES_TO_CONTEXT_TIME_ZONE
+        //
+        ObjectReader reader2 = newMapper().disable(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE).readerFor(OffsetDateTime.class);
+        OffsetDateTime parsed = reader2.readValue(aposToQuotes("'2000-01-01T12:00+05:00'"));
+        notNull(parsed);
+        expect(OffsetDateTime.of(2000, 1, 1, 12, 0, 0, 0, ZoneOffset.ofHours(5)), parsed) ;
     }
 
     @Test
